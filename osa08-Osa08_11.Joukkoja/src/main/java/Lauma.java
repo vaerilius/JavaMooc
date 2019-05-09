52bb23f1e54141207fcdab61199e6bc3b13470ef
Vaerilius <tjohannest@gmail.com>
2019-05-09 10:40:45 +0300

    test

--- osa08-Osa08_11.Joukkoja/src/main/java/Lauma.java
+++ osa08-Osa08_11.Joukkoja/src/main/java/Lauma.java
@@ -5,7 +5,7 @@ public class Lauma implements Siirrettava{
 
     @Override
     public void siirra(int dx, int dy) {
-        System.out.println("test push");        System.out.println("test push");
+        System.out.println("test push");       
 
     }
     
