13163e4b36e56b6be17172fd98ec06cf985a7751
Vaerilius <tjohannest@gmail.com>
2019-05-09 11:26:13 +0300

    11 done

--- osa08-Osa08_11.Joukkoja/src/main/java/Lauma.java
+++ osa08-Osa08_11.Joukkoja/src/main/java/Lauma.java
@@ -1,12 +1,37 @@
 
-public class Lauma implements Siirrettava{
-    
-    
+import java.util.ArrayList;
+import java.util.List;
 
+public class Lauma implements Siirrettava {
+    
+    private List<Siirrettava> lauma;
+    
+    public Lauma() {
+        this.lauma = new ArrayList<>();
+    }
+    
     @Override
     public void siirra(int dx, int dy) {
-        System.out.println("test push");       
-
+        
+        for (Siirrettava siirrettava : lauma) {
+            siirrettava.siirra(dx, dy);
+        }
+        
+    }
+    
+    @Override
+    public String toString() {
+        String sijainnit = "";
+        for (Object elio : lauma) {
+            sijainnit += elio.toString() + "\n";
+        }
+        
+        return sijainnit;
+    }
+    
+    public void lisaaLaumaan(Siirrettava siirrettava) {
+        
+        this.lauma.add(siirrettava);
     }
     
 }
--- osa08-Osa08_11.Joukkoja/src/main/java/Paaohjelma.java
+++ osa08-Osa08_11.Joukkoja/src/main/java/Paaohjelma.java
@@ -2,11 +2,11 @@
 public class Paaohjelma {
 
     public static void main(String[] args) {
-        Elio elio = new Elio(20, 30);
-        System.out.println(elio);
-        elio.siirra(-10, 5);
-        System.out.println(elio);
-        elio.siirra(50, 20);
-        System.out.println(elio);
+        Lauma lauma = new Lauma();
+        lauma.lisaaLaumaan(new Elio(73, 56));
+        lauma.lisaaLaumaan(new Elio(57, 66));
+        lauma.lisaaLaumaan(new Elio(46, 52));
+        lauma.lisaaLaumaan(new Elio(19, 107));
+        System.out.println(lauma);
     }
 }
