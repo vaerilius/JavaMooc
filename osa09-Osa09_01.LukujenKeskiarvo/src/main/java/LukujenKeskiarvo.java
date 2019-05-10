ca056d65f8ea5b7818486208ba9ff272d3bfc05a
Vaerilius <tjohannest@gmail.com>
2019-05-10 08:54:27 +0300

    streaming()

--- osa09-Osa09_01.LukujenKeskiarvo/src/main/java/LukujenKeskiarvo.java
+++ osa09-Osa09_01.LukujenKeskiarvo/src/main/java/LukujenKeskiarvo.java
@@ -1,12 +1,25 @@
 
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class LukujenKeskiarvo {
 
     public static void main(String[] args) {
         Scanner lukija = new Scanner(System.in);
-        // toteuta ohjelmasi tänne
+        List<String> list = new ArrayList<>();
 
+        while (true) {
+            String syote = lukija.nextLine();
+            if (syote.equals("loppu")) {
+                break;
+            }
+            list.add(syote);
+        }
+        double average = list.stream()
+                .mapToInt(luku -> Integer.valueOf(luku))
+                .average()
+                .getAsDouble();
+        System.out.println("Lukujen keskiarvo: " + average);
     }
 }
--- osa09-Osa09_02.TiettyjenLukujenKeskiarvo/src/main/java/TiettyjenLukujenKeskiarvo.java
+++ osa09-Osa09_02.TiettyjenLukujenKeskiarvo/src/main/java/TiettyjenLukujenKeskiarvo.java
@@ -1,12 +1,40 @@
 
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class TiettyjenLukujenKeskiarvo {
 
     public static void main(String[] args) {
         Scanner lukija = new Scanner(System.in);
-        // toteuta ohjelmasi tänne
+        List<String> list = new ArrayList<>();
+
+        while (true) {
+            String syote = lukija.nextLine();
+            if (syote.equals("loppu")) {
+                break;
+            }
+            list.add(syote);
+        }
+
+        System.out.println("Tulostetaanko negatiivisten vai positiivisten lukujen keskiarvo? (n/p)");
+        String vastaus = lukija.nextLine();
+
+        if (vastaus.equals("n")) {
+            double nega = list.stream()
+                    .mapToInt(luku -> Integer.valueOf(luku))
+                    .filter(filt -> filt < 0)
+                    .average()
+                    .getAsDouble();
+            System.out.println("Negatiivisten lukujen keskiarvo: " + nega);
+        } else {
+            double pos = list.stream()
+                    .mapToInt(luku -> Integer.valueOf(luku))
+                    .filter(filt -> filt > 0)
+                    .average()
+                    .getAsDouble();
+            System.out.println("Positiivisten lukujen keskiarvo: " + pos);
+        }
 
     }
 }
