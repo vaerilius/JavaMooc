79511fecb45445c45179b5b903954d51823de27e
Vaerilius <tjohannest@gmail.com>
2019-05-11 09:09:22 +0300

    a

--- osa09-Osa09_05.LuettujenArvojenTulostaminen/src/main/java/LuettujenArvojenTulostaminen.java
+++ osa09-Osa09_05.LuettujenArvojenTulostaminen/src/main/java/LuettujenArvojenTulostaminen.java
@@ -6,6 +6,18 @@ public class LuettujenArvojenTulostaminen {
 
     public static void main(String[] args) {
         Scanner lukija = new Scanner(System.in);
+        ArrayList<String> lista = new ArrayList();
+        
+        while (true) { 
+            String arvo = lukija.nextLine();
+            if (arvo.equals("")) {
+                break;
+            }
+            lista.add(arvo);
+        }
+        lista.stream()
+                .forEach(arvo -> System.out.println(arvo));
 
     }
+
 }
