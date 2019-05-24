5c73315995c22508e5c08e3cb661c7f222db8db4
Vaerilius <tjohannest@gmail.com>
2019-05-24 21:13:30 +0300

    9-10

--- osa12-Osa12_09.UseampiNakyma/src/main/java/sovellus/UseampiNakyma.java
+++ osa12-Osa12_09.UseampiNakyma/src/main/java/sovellus/UseampiNakyma.java
@@ -1,9 +1,58 @@
 package sovellus;
 
-public class UseampiNakyma {
+import javafx.application.Application;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+public class UseampiNakyma extends Application {
 
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Application.launch(UseampiNakyma.class);
     }
 
+    @Override
+    public void start(Stage w) {
+
+        BorderPane eka = new BorderPane();
+        Label text = new Label("Eka näkymä!");
+        Button tokaan = new Button("Tokaan näkymään!");
+        eka.setCenter(tokaan);
+        eka.setTop(text);
+        Scene ekaScene = new Scene(eka);
+
+        VBox toka = new VBox();
+        Button kolmanteen = new Button("Kolmanteen näkymään!");
+        Label text2 = new Label("Toka näkymä!");
+        Scene tokaScene = new Scene(toka);
+        toka.getChildren().addAll(kolmanteen, text2);
+
+        GridPane kolmas = new GridPane();
+        Button ekaan = new Button("Ekaan näkymään!");
+
+        kolmas.add(new Label("Kolmas näkymä!"), 0, 0);
+        kolmas.add(ekaan, 1, 1);
+        Scene kolmasScene = new Scene(kolmas);
+
+        ekaan.setOnAction((event) -> {
+
+            w.setScene(ekaScene);
+        });
+
+        tokaan.setOnAction((event) -> {
+            w.setScene(tokaScene);
+        });
+
+        kolmanteen.setOnAction((event) -> {
+            w.setScene(kolmasScene);
+        });
+
+        w.setScene(ekaScene);
+        w.show();
+    }
 }
--- osa12-Osa12_10.Tervehtija/src/main/java/sovellus/TervehtijaSovellus.java
+++ osa12-Osa12_10.Tervehtija/src/main/java/sovellus/TervehtijaSovellus.java
@@ -1,9 +1,61 @@
 package sovellus;
 
-public class TervehtijaSovellus {
+import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class TervehtijaSovellus extends Application{
 
 
     public static void main(String[] args) {
-        System.out.println("Hellow world!");
+        launch(TervehtijaSovellus.class);
+    }
+
+    @Override
+    public void start(Stage w) throws Exception {
+        Label teksti = new Label("Kirjoita nimesi ja aloita.");
+        TextField field = new TextField();
+        Button button = new Button("Aloita");
+        
+        GridPane grid = new GridPane();
+        
+        grid.setPrefSize(300, 180);
+        grid.setAlignment(Pos.CENTER);
+        grid.setVgap(10);
+        grid.setHgap(10);
+        
+        grid.setPadding(new Insets(10, 10, 10, 10));
+        
+        grid.add(teksti, 0, 0);
+        grid.add(field, 0, 1);
+        grid.add(button, 0, 2);
+        
+        Scene view = new Scene(grid);
+        
+        StackPane stack = new StackPane();
+         Label tervetuloaTeksti = new Label("Tervetuloa ");
+         stack.setPrefSize(300, 180);
+         stack.getChildren().add(tervetuloaTeksti);
+         stack.setAlignment(Pos.CENTER);
+        
+        Scene view2 = new Scene(stack);
+        
+        button.setOnAction((event) -> {
+            tervetuloaTeksti.setText("Tervetuloa " + field.getText() + "!");
+            w.setScene(view2);
+        });
+        
+        w.setScene(view);
+        
+        w.show();
+        
+        
     }
 }
