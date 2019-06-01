0d1645182af0b086011b216fbcb47908037a1d52
Vaerilius <39900747+vaerilius@users.noreply.github.com>
2019-06-01 09:40:25 +0300

    asteroids

---
+++ osa13-Osa13_09.Asteroids/src/main/java/asteroids/Alus.java
@@ -0,0 +1,12 @@
+package asteroids;
+
+import javafx.scene.shape.Polygon;
+
+public class Alus extends Hahmo {
+    
+    public Alus(int x, int y) {
+        super(new Polygon(-5, -5, 10, 0, -5, 5), x, y);
+        
+    }
+    
+}
---
+++ osa13-Osa13_09.Asteroids/src/main/java/asteroids/Ammus.java
@@ -0,0 +1,12 @@
+
+package asteroids;
+
+import javafx.scene.shape.Polygon;
+
+public class Ammus extends Hahmo{
+    
+    public Ammus( int x, int y) {
+          super(new Polygon(2, -2, 2, 2, -2, 2, -2, -2), x, y);
+    }
+    
+}
